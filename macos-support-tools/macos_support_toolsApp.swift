3e19666ecd8edc8712c7b64a4d8a5c657482efe9
Jacob Massih <massih.jacob@gmail.com>
2025-09-26 20:56:07 -0400

    Initial Commit

---
+++ macos-support-tools/macos_support_toolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_support_toolsApp.swift
+//  macos-support-tools
+//
+//  Created by Jacob Massih on 2025-09-26.
+//
+
+import SwiftUI
+
+@main
+struct macos_support_toolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macos-support-toolsUITests/macos_support_toolsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  macos_support_toolsUITestsLaunchTests.swift
+//  macos-support-toolsUITests
+//
+//  Created by Jacob Massih on 2025-09-26.
+//
+
+import XCTest
+
+final class macos_support_toolsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
