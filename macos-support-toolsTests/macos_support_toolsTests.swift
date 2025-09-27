6dd607ac4d778e473bc29468e825efe6b5705df7
Jacob Massih <67515993+jacobmassih@users.noreply.github.com>
2025-09-26 20:56:07 -0400

    Initial Commit

---
+++ macos-support-toolsTests/macos_support_toolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  macos_support_toolsTests.swift
+//  macos-support-toolsTests
+//
+//  Created by Jacob Massih on 2025-09-26.
+//
+
+import Testing
+@testable import macos_support_tools
+
+struct macos_support_toolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
