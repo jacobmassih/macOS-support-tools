c3169e9cd8121b83759c7cca2c141c085d888a2e
Jacob Massih <massih.jacob@gmail.com>
2026-06-14 14:33:18 -0400

    Update menu bar icon

--- macos-support-tools/App/macos_support_toolsApp.swift
+++ macos-support-tools/App/macos_support_toolsApp.swift
@@ -6,7 +6,7 @@ struct macos_support_toolsApp: App {
     @State private var cleanupManager = CleanupManager()
 
     var body: some Scene {
-        MenuBarExtra("Support Tools", systemImage: "computermouse") {
+        MenuBarExtra("Support Tools", systemImage: "wrench.and.screwdriver") {
             MenuBarManager()
                 .environment(mouseManager)
         }
